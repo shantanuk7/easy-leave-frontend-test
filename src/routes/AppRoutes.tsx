447b566a2ec93f68c1ad8dc63eb87d82b9e87dd6
RajDeshmukh2001 <rajdd61@gmail.com>
2026-03-31 12:11:37 +0530

    feat(LMS-7): update routing add Home component as index page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,16 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from '@/components/Layout';
 import Leave from '@/pages/Leave';
 import Dashboard from '@/pages/Dashboard';
+import Home from '@/components/Home';
+
 const AppRoutes = (): React.JSX.Element => {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path='/' element={<div>Home</div>} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/leave' element={<Leave />} />
       </Route>
+      <Route index path="/" element={<Home />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default AppRoutes;
