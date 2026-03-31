f7b4d109183483090bb935ccd5a9d1c6f22c9d9a
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-31 14:31:54 +0530

    feat: create a useLeave hook to call api and return laoding, error and leaves

---
+++ src/hooks/useLeaves.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { fetchLeaves } from "../api/leave.api";
+import type { LeaveScope, LeaveStatus } from "@/constants/LeaveStatus";
+import type { LeaveResponse } from "@/types/leaves";
+
+function useLeaves(status: LeaveStatus, scope: LeaveScope) {
+  const [leaves, setLeaves] = useState<LeaveResponse[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function loadLeaves() {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const data = await fetchLeaves({ status, scope });
+        setLeaves(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadLeaves();
+  }, [status, scope]);
+
+  return { leaves, loading, error };
+}
+
+export default useLeaves;
