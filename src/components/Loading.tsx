b3608e65747574021d6379c0b679267bb6fb8b9c
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-06 15:13:07 +0530

    refactor(lms-19): update the loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,13 @@
-import { Loader } from 'lucide-react';
+import { Loader2 } from 'lucide-react';
 import React from 'react';
 
-function Loading() : React.JSX.Element {
+function Loading(): React.JSX.Element {
   return (
-    <div className='text-center w-full flex flex-col justify-center items-center'>
-      <Loader />
+    <div className="text-center w-full flex flex-col justify-center items-center">
+      <Loader2 className="animate-spin text-gray-500" size={34} />
       <p className="ml-2">Loading...</p>
     </div>
-  )
+  );
 }
 
 export default Loading;
