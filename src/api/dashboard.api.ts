9e327d9881c2f7fc96f694e0c30b212cfb52e445
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-09 18:54:00 +0530

    fix(lms-72): remove the console statement in api call

--- src/api/dashboard.api.ts
+++ src/api/dashboard.api.ts
@@ -6,6 +6,5 @@ export const getManagerDashboardMetrics = async (): Promise<ManagerDashboardMetr
   if (!data.success) {
     throw new Error(data.message || 'Failed to fetch dashboard metrics');
   }
-  console.log('Dashboard Metrics:', data.data);
   return data.data;
 };
