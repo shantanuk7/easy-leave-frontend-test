d5def80a8776d4e6c8f3cb217e2bbe2ca83aa801
RajDeshmukh2001 <rajdd61@gmail.com>
2026-04-09 22:35:27 +0530

    refactor(LMS-27): move useLeaveCategories from ApplyLeaveForm to LeaveForm

--- src/components/ApplyLeaveForm.tsx
+++ src/components/ApplyLeaveForm.tsx
@@ -5,7 +5,6 @@ import { applyLeave } from '@/api/leave.api';
 import type { LeaveApplicationRequest } from '@/types/leaves';
 import { getDatesBetween } from '@/utils/time';
 import { isAxiosError } from 'axios';
-import useLeaveCategories from '@/hooks/useLeaveCategories';
 import LeaveForm from './LeaveForm';
 import type { LeaveFormValues } from '@/types/leaveForm';
 
@@ -22,8 +21,6 @@ const ApplyLeaveForm = ({
 }: {
   refreshLeaves: () => Promise<void>;
 }): React.JSX.Element => {
-  const { categories, loading, error } = useLeaveCategories();
-
   const handleSubmit = async (
     values: LeaveFormValues,
     { resetForm }: FormikHelpers<LeaveFormValues>,
@@ -50,15 +47,7 @@ const ApplyLeaveForm = ({
     }
   };
 
-  return (
-    <LeaveForm
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      categories={categories}
-      categoriesLoading={loading}
-      categoriesError={error}
-    />
-  );
+  return <LeaveForm initialValues={initialValues} onSubmit={handleSubmit} />;
 };
 
 export default ApplyLeaveForm;
