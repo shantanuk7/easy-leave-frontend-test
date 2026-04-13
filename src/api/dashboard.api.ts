50c573950d92b813a2a60cffbaab450b7946dff4
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-13 10:19:48 +0530

    fix(lms-72): add api response type in dashboard api

--- src/api/dashboard.api.ts
+++ src/api/dashboard.api.ts
@@ -1,8 +1,10 @@
 import type { ManagerDashboardMetrics } from '@/types/dashboard';
 import axiosInstance from './axiosInstance';
+import type { ApiResponse } from '@/types/response';
 
 export const getManagerDashboardMetrics = async (): Promise<ManagerDashboardMetrics> => {
-  const { data } = await axiosInstance.get('/api/dashboard/manager');
+  const { data } =
+    await axiosInstance.get<ApiResponse<ManagerDashboardMetrics>>('/api/dashboard/manager');
   if (!data.success) {
     throw new Error(data.message || 'Failed to fetch dashboard metrics');
   }
