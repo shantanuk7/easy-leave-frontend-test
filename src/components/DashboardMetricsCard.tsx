70beff66131b962564a7a42ed01d2bc470eb4cb4
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-13 10:25:02 +0530

    fix(lms-72): add twMerge in DashboardMetricsCard

--- src/components/DashboardMetricsCard.tsx
+++ src/components/DashboardMetricsCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { twMerge } from 'tailwind-merge';
 type DashboardMetricsCardProps = {
   title: string;
   value: string | number;
@@ -14,7 +14,9 @@ function DashboardMetricsCard({
   style,
 }: DashboardMetricsCardProps): React.JSX.Element {
   return (
-    <div className={`px-4 py-4 border border-gray-200 rounded-2xl bg-white shadow-sm ${style}`}>
+    <div
+      className={twMerge('px-4 py-4 border border-gray-200 rounded-2xl bg-white shadow-sm', style)}
+    >
       <div className="flex justify-between items-center">
         <p className="font-medium text-gray-700">{title}</p>
         <div className="text-gray-500 flex items-center justify-center">{icon}</div>
